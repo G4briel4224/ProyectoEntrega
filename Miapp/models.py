510f150c75b6b5b368c01ab4a6cea176a9d59795
Gabriel Quintero <g4briel.qu1nt3ro@gmail.com>
2025-06-11 21:59:57 -0400

    Segundo intento

---
+++ Miapp/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    email = models.EmailField()
+  
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=100)
+    contenido = RichTextField()
+    imagen = models.ImageField(upload_to='autores/')
+    fecha = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
